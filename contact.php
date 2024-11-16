52a0ac83b65b4677679621996e041ac7e48e3cbc
Pamela Nicole Magcawas <magcawaspamelanicole@gmail.com>
2024-11-16 20:30:47 +0800

    updated contact page

--- contact.php
+++ contact.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us - Animal Adoption</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Contact Us</h1>
-    <p>
-        We truly value the support and engagement from animal lovers, advocates and those passionate about improving the lives of animals. Whether you are 
-        interested in adoptiong, making a donation, or simply reaching out with a question or feedback, we are eager to hear from you! We understand how important
-        communication is, and we strive to respond to all inquiries as quickly as possible.
-    </p>
+    <div class="heading">
+        <h1>Contact Us</h1>
+        <p>
+            We truly value the support and engagement from animal lovers, advocates and those passionate about improving the lives of animals. Whether you are 
+            interested in adoptiong, making a donation, or simply reaching out with a question or feedback, we are eager to hear from you! We understand how important
+            communication is, and we strive to respond to all inquiries as quickly as possible.
+        </p>
+    </div>
 
     <form action="" method="post">
-        <label for="name">Full Name:</label>
+        <label for="name">Full Name:</label><br>
         <input type="text" id="name" name="name" required><br><br>
 
         <label for="email">Email:</label><br>
