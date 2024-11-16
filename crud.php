55cd8eda96d89a672e7e15d7486f9953808d6697
Pamela Nicole Magcawas <magcawaspamelanicole@gmail.com>
2024-11-16 20:42:47 +0800

    for crud operations

---
+++ crud.php
@@ -0,0 +1,42 @@
+<?php
+
+class Animal {
+    private $conn;
+    private $tbl_name = "animaldata";
+
+    public $id;
+    public $name;
+    public $age;
+    public $sex;
+    public $treatments;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function create(){
+        $query = "INSERT INTO " .$this->tbl_name . " (Name, Age, Sex, Treatments) VALUES (:name, :age, :sex, :treatments)";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':age', $this->age);
+        $stmt->bindParam(':sex', $this->sex);
+        $stmt->bindParam(':treatments', $this->treatments);
+
+        if($stmt->execute()){
+            return true;
+        }
+        
+        return false;
+    }
+
+    //get data from database and display
+    public function read(){
+        $query = "SELECT * FROM " .$this->tbl_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
+?>
