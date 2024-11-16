341927896d3e35fb28656bc9b61ae221b8e12f73
Banduluu <159671463+Banduluu@users.noreply.github.com>
2024-11-16 01:49:50 -0800

    try lang po

---
+++ donate.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animal Adoption</title>
+    <link rel="stylesheet" href="status.css">
+</head>
+<body>
+
+
+    <nav class="port">
+        <ul>
+            <li><a href="index.php" class="active">Home</a></li>
+            <li><a href="#">Our Animals</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div>
+            <div class = "spaces">
+        <p>Please donate when you can. Every little thing helps for our rescues. Seeing them smile and then sleep so well after a good meal makes all the effort and struggles worth   
+ it.</p>
+            </div>
+
+            
+        
+    </div>
+            <div class="web">
+
+
+        <div class="image-columns">
+
+<div class="column">
+    <img src="" alt="Image 1">
+    <img src="" alt="Image 2">
+    <img src="" alt="Image 3">
+    <img src="" alt="Image 3">
+</div>
+        <h2>OFFICIAL DONATION CHANNEL</h2>
+    </div>
+            
+
+        </div>
+        </div>
+    </main>
+
+</body>
+</html>
