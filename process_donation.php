cef0aa4bfd1afef030e563d091639f7f556b66c1
Banduluu <159671463+Banduluu@users.noreply.github.com>
2024-11-27 06:58:27 -0800

    Update

--- process_donation.php
+++ process_donation.php
@@ -1,22 +1,53 @@
 <?php
-include 'db_donation.php'; 
+require_once 'db_donation.php';
+require_once 'donations.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $donation_amount = $_POST['donation_amount'];
-    $payment_method = $_POST['payment_method'];
+    $database = new Database();
+    $db = $database->getConnect();
 
-    $sql = "INSERT INTO Donations (first_name, last_name, email, donation_amount, payment_method)
-            VALUES ('$first_name', '$last_name', '$email', '$donation_amount', '$payment_method')";
+    $donations = new Donations($db);
+    $donations->name = htmlspecialchars(trim($_POST['name']));
+    $donations->email = htmlspecialchars(trim($_POST['email']));
+    $donations->donation_amount = htmlspecialchars(trim($_POST['donation_amount']));
+    $donations->payment_method = htmlspecialchars(trim($_POST['payment_method'])); 
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Donation recorded successfully!";
+    if ($donations->processDonation()) {
+        echo "
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>SweetAlert</title>
+            <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+        </head>
+        <body>
+        <script>
+        Swal.fire({
+            title: 'Donation Successful!',
+            text: 'You’ve Made a Paw-sitive Difference!',
+            icon: 'success'
+        }).then(() => {
+            window.location.href = 'donate.php';
+        });
+        </script>";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "
+        <script>
+        Swal.fire({
+            title: 'Error!',
+            text: 'Unable to process your donation. Please try again.',
+            icon: 'error'
+        }).then((result) => {
+            if(result.isConfirmed) {
+                window.location.href = 'donate.php';
+            }
+        });
+        </script>
+
+        </body>
+        </html> ";
     }
 }
-
-$conn->close();
 ?>
